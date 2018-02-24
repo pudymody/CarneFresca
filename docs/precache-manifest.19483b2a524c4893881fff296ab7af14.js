efe5f25f397e0d10f8bb08f286ac180433c96825
Federico Scodelaro <federicoscodelaro@gmail.com>
2018-02-23 22:34:09 -0300

    Build

---
+++ docs/precache-manifest.19483b2a524c4893881fff296ab7af14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "295f5a6094ecfd937797",
+    "url": "/js/vendor.295f5a60.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "712cc5a2b09e6c37c514",
+    "url": "/js/app.712cc5a2.js"
+  },
+  {
+    "revision": "45835fdc1c04ad92cda06f8138c0ea9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "712cc5a2b09e6c37c514",
+    "url": "/css/app.4aec4594.css"
+  }
+];
